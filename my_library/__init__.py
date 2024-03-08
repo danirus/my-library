0adee00b8b68ff543ad944401394fd57cb12e129
Daniela Rus Morales <danirus@eml.cc>
2024-03-08 12:40:29 +0100

    Use GHA environment files

--- my_library/__init__.py
+++ my_library/__init__.py
@@ -1,13 +1,10 @@
-import pkg_resources
-
-
 VERSION = (1, 0, 0, 'f', 0)
 
 
 def get_release():
     release = '%s.%s.%s' % (VERSION[0], VERSION[1], VERSION[2])
     if VERSION[3] != 'f':
-        release = '%s%s%s' % (version, VERSION[3], VERSION[4])
+        release = '%s%s%s' % (release, VERSION[3], VERSION[4])
     return release
 
 def get_version():
